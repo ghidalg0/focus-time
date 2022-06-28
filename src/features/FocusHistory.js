4d82f91c6e02e3cec9086e0875bbcebf3d1c1514
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-28 15:34:46 +0200

    initial commit // focus time app - react native

---
+++ src/features/FocusHistory.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { colors } from '../utils/colors';
+import { fontSizes, spacing } from '../utils/sizes';
+
+export const FocusHistory = ({ history }) => {
+  if (!history || !history.length) return <Text style={styles.title2}>We haven't focused yet ! Get started baby</Text>;
+
+  const renderItem = ({ item }) => <Text style={styles.item}> •  {item}</Text>;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Things we've focused on:</Text>
+      <FlatList data={history} renderItem={renderItem} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: spacing.md,
+    flex: 1,
+  },
+  item: {
+    color: colors.orange,
+    fontSize: fontSizes.md,
+    paddingLeft: spacing.md,
+    paddingTop: spacing.sm,
+  },
+  title: {
+    color: colors.white,
+    fontSize: fontSizes.md,
+    fontWeight: 'bold',
+  },
+  title2: {
+    color: colors.white,
+    fontSize: fontSizes.md,
+    fontWeight: 'bold',
+    paddingTop: 50,
+    textAlign: 'center',
+  }
+});
---
+++ src/utils/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+  white: '#ffffff',
+  darkBlue: '#023047',
+  orange: '#FC8500',
+  yellow: '#FFB706'
+}
